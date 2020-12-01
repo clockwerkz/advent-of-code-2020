acfc546fcd59d4e6a7aec12c58b16adbb812db2e
Carlos <clockwerkz@gmail.com>
2020-12-01 10:38:33 -0800

    testing seet up and configured

--- day_1/test.js
+++ day_1/test.js
@@ -1,16 +1,15 @@
 const assert = require('assert');
-const { isMainThread } = require('worker_threads');
 const one = require('./one');
 
 describe("Day 1 - Part 1", ()=>{
     const test = "test";
     it("sets up testing", ()=>{
-        assert(test).equals("test");
+        assert.strictEqual(test, "test");
     })
 });
-let input = `1721
-979
-366
-299
-675
-1456`;
+// let input = `1721
+// 979
+// 366
+// 299
+// 675
+// 1456`;
