246e80a7532257d1ec3b9feaa82a9b74301cb181
Carlos <clockwerkz@gmail.com>
2020-12-03 11:23:25 -0800

    sets up folder/files for day 3

---
+++ day_3/test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const one = require('./one');
+const two = require('./two');
+
+describe("Day 3 - Part 1", ()=>{  
+    const input = "";
+    it("should return return 0", ()=>{
+        assert.strictEqual(one(input), 0);
+    });
+});
