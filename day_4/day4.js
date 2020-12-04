a5c3be10c71b6eccfd3e70c2006528ac6f10c98e
Carlos <clockwerkz@gmail.com>
2020-12-04 11:12:16 -0800

    part one competed - needs refactoring

---
+++ day_4/day4.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const one = require('./one');
+const two = require('./two');
+
+fs.readFile('input.txt', 'utf8', (err, data)=>{
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(one(data));
+})
---
+++ day_4/one.js
@@ -0,0 +1,11 @@
+module.exports = function(input) {
+    const fields = [ "byr","iyr","eyr","hgt","hcl","ecl","pid"];
+    return input
+        .split(/\n\s*\n/)
+        .map(el => el.trim())
+        .reduce((validPassports, passport)=>{
+            entries = passport.split(/\n*\s+/).map(el => el.split(":")[0]);
+            let matchingFields = entries.reduce((matching, field) => fields.indexOf(field)!== -1 ? matching + 1 : matching, 0);
+            return matchingFields == fields.length ? validPassports + 1 : validPassports;
+        },0)
+}
