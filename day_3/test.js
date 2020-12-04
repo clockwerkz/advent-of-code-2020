1c7e626777297327e631571f1599fb55f15c7e66
Carlos <clockwerkz@gmail.com>
2020-12-03 17:46:12 -0800

    refactoring one's solution for part two

--- day_3/test.js
+++ day_3/test.js
@@ -18,3 +18,20 @@ describe("Day 3 - Part 1", ()=>{
         assert.strictEqual(one(input), 7);
     });
 });
+
+describe("Day 3 - Part 2", ()=>{  
+    const input = `..##.......
+    #...#...#..
+    .#....#..#.
+    ..#.#...#.#
+    .#...##..#.
+    ..#.##.....
+    .#.#.#....#
+    .#........#
+    #.##...#...
+    #...##....#
+    .#..#...#.#`;
+    it("should return the number of #s on the path of slope right 3, down 1: 7", ()=>{
+        assert.strictEqual(two(input), 7);
+    });
+});
--- day_3/two.js
+++ day_3/two.js
@@ -1,3 +1,11 @@
 module.exports = function(input) {
-    return 0;
+    const matrix = input.split('\n').map(el => el.trim());
+    let cols = matrix[0].length;
+    let colPosition = 0;
+    let treeCount = 0;
+    for (let i=1; i < matrix.length; i++) {
+        colPosition = (colPosition + 3) % cols;
+        if (matrix[i][colPosition] == "#") treeCount++; 
+    }
+    return treeCount;
 }
