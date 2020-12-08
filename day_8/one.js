b0c41926e9049ae55c3b3edf5ceb3800992661eb
clockwerkz <clockwerkz@gmail.com>
2020-12-07 22:34:57 -0800

    Day 8 part 1 complete!

--- day_8/one.js
+++ day_8/one.js
@@ -1,3 +1,26 @@
 module.exports = function(input) {
+    const repeatedCode = {};
+    const code = input.split('\n').map(el=> el.trim());
+    let acc = 0;
+    let i = 0;
+    while (true) {
+        if (repeatedCode[i]) {
+            return acc;
+        }
+        repeatedCode[i] = 1;
+        let [ instruction, val ] = code[i].split(' ');
+        switch (instruction) {
+            case "acc":
+                acc += parseInt(val);
+                i++;
+                break;
+            case "jmp":
+                i+=parseInt(val);
+                break;
+            case "nop":
+            default:
+                i++;
+        }
+    }
     return 0;
 }
--- day_8/test.js
+++ day_8/test.js
@@ -4,15 +4,24 @@ const one = require("./one");
 const two = require("./two");
 
 describe("Day 8 - Part 1",()=>{
-    const answer = 0;
+    const input = `nop +0
+    acc +1
+    jmp +4
+    acc +3
+    jmp -3
+    acc -99
+    acc +1
+    jmp -4
+    acc +6`;
+    const answer = 5;
     it(`should return ${answer}`,()=>{
-        assert.strictEqual(one(), answer);
+        assert.strictEqual(one(input), answer);
     });
 });
 
 describe("Day 8 - Part 2",()=>{
     const answer = 0;
     it(`should return ${answer}`,()=>{
-        assert.strictEqual(one(), answer);
+        assert.strictEqual(two(), answer);
     });
 });
