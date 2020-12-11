a14443d9bd3a7a9e0dbd4c6d90c1c38f0cd37d00
clockwerkz <clockwerkz@gmail.com>
2020-12-11 00:06:31 -0800

    working through Day 10 - part 2

--- day_10/test.js
+++ day_10/test.js
@@ -42,8 +42,54 @@ describe("Day 10 - Part 1",()=>{
 });
 
 describe("Day 10 - Part 2",()=>{
-    const answer = 0;
-    it(`should return ${answer}`,()=>{
-        assert.strictEqual(two(), answer);
+    input = `16
+    10
+    15
+    5
+    1
+    11
+    7
+    19
+    6
+    12
+    4`;
+    const answer = 8;
+    const input2 = `28
+    33
+    18
+    42
+    31
+    14
+    46
+    20
+    48
+    47
+    24
+    23
+    49
+    45
+    19
+    38
+    39
+    11
+    1
+    32
+    25
+    35
+    8
+    17
+    7
+    9
+    4
+    2
+    34
+    10
+    3`;
+    const answer2 = 19208;
+    it(`should return the number of permutations possible where each number is within 3: ${answer}`,()=>{
+        assert.strictEqual(two(input), answer);
+    });
+    it(`should return the number of permutations possible where each number is within 3: ${answer2}`,()=>{
+        assert.strictEqual(two(input2), answer2);
     });
 });
--- day_10/two.js
+++ day_10/two.js
@@ -1,3 +1,19 @@
 module.exports = function(input) {
-    return 0;
+    const arr = input.split('\n').map(el => parseInt(el)).sort((a,b) => a - b);
+    arr.push(arr[arr.length -1] + 3);
+    arr.unshift(0);
+    let perms = 0;
+    let i = 0;
+    while (i < arr.length - 3) {
+        if (arr[i + 3] - arr[i] <= 3) {
+            perms *= 4;
+            i+=3;
+        } else if (arr[i+2] - arr[i] <= 3) {
+            perms *= 2;
+            i+=2;
+        } else {
+            i++;
+        }
+    }
+    return perms;
 }
