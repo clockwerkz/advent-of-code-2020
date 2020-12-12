b3a38339871bcd4020e20e76dad475385c62f22c
Carlos <clockwerkz@gmail.com>
2020-12-11 21:12:12 -0800

    day 11 initial setup

---
+++ day_11/test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+
+const one = require("./one");
+const two = require("./two");
+
+describe("Day # - Part 1",()=>{
+    const input = `L.LL.LL.LL
+    LLLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLLL
+    L.LLLLLL.L
+    L.LLLLL.LL`;
+    const answer = 0;
+    it(`should return ${answer}`,()=>{
+        assert.strictEqual(one(), answer);
+    });
+});
+
+describe("Day # - Part 2",()=>{
+    const answer = 0;
+    it(`should return ${answer}`,()=>{
+        assert.strictEqual(two(), answer);
+    });
+});
